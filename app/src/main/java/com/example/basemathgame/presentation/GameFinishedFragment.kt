b002d8880185b1d5d84d9a380e9cbe6261835879
GlebRTA <104761917+GlebRTA@users.noreply.github.com>
2023-02-02 16:44:13 +0200

    Finished 02.02.2023

--- app/src/main/java/com/example/basemathgame/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/basemathgame/presentation/GameFinishedFragment.kt
@@ -1,17 +1,14 @@
 package com.example.basemathgame.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.basemathgame.R
-import com.example.basemathgame.databinding.FragmentChoosLevelBinding
 import com.example.basemathgame.databinding.FragmentGameFinishedBinding
-import com.example.basemathgame.domain.entity.GameResult
 
 class GameFinishedFragment : Fragment() {
 
